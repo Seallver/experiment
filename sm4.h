085e6298c21c68e8a9bceb0e0bc33015b1fd8b02
seallver <3211523143@qq.com>
2025-07-07 15:09:02 +0800

    实现轮密钥生成和S盒变换

---
+++ sm4.h
@@ -0,0 +1,21 @@
+#ifndef SM4_H
+#define SM4_H
+
+#include "stdint.h"
+#include "stdio.h"
+
+//轮密钥
+typedef struct sm4_key {
+  uint32_t rk[32];
+} SM4_Key;
+
+//初始化密钥
+void sm4_keyInit(uint8_t *key, SM4_Key *sm4_key);
+
+//加密函数
+void sm4_encrypt(const uint8_t *input, const SM4_Key *key, uint8_t *output);
+
+//解密函数
+void sm4_decrypt(const uint8_t *input, const SM4_Key *key, uint8_t *output);
+
+#endif
